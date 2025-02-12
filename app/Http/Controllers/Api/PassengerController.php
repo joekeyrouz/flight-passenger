310ee9450b9517940f6d810a41a8bb73277e9971
unknown <joekeyrouz03@gmail.com>
2025-02-12 14:45:59 +0200

    Added my project

--- app/Http/Controllers/Api/PassengerController.php
+++ app/Http/Controllers/Api/PassengerController.php
@@ -6,21 +6,18 @@ use App\Models\Flight;
 use App\Models\Passenger;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\Builder;
 
 class PassengerController extends Controller
 {
     public function index(Flight $flight){
-        $flight = Flight::with('passengers')->findOrFail($flight->id);
-        return response()->json($flight->passengers);
+        $passengers = Passenger::whereHas('flights', function (Builder $query) use($flight){
+            $query->where('flight_id', "{$flight->id}");
+        })->get();
+        return response()->json($passengers);
     }
 
     public function show(Request $req,Flight $flight,Passenger $passenger){
-        $flight = Flight::with('passengers')->findOrFail($flight->id);
-        foreach($flight->passengers as $pass){
-            if($pass['id'] == $passenger['id']){
-                return response()->json($passenger);
-            }
-        }
-        return response()->json([]);
+        return response()->json($passenger);
     }
 }
