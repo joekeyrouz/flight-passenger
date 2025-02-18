41045978f93f735527569b0d26cb10ab8426d428
unknown <joekeyrouz03@gmail.com>
2025-02-18 12:51:14 +0200

    Added my project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,37 +19,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // $adminRole = Role::create(['name' => 'admin']);
-        // $userRole = Role::create(['name' => 'user']);
-
-        // $admin = User::create([
-        //     'name' => 'admin',
-        //     'email' => 'admin@gmail.com',
-        //     'password' => 'admin12345'
-        // ]);
-        // $admin->assignRole($adminRole);
-
-        // $user = User::create([
-        //     'name' => 'Regular User',
-        //     'email' => 'user@gmail.com',
-        //     'password' => 'user12345',
-        // ]);
-        // $user->assignRole($userRole);
-
         $this->call([
             RoleSeeder::class,
             PassengerSeeder::class,
             FlightSeeder::class,
         ]);
-
-        // Passenger::factory(1000)->create();
-        // Flight::factory(50)->create();
-        
-        // Flight::all()->each(function ($flight) {
-        //     $passengers = Passenger::inRandomOrder()->take(rand(1, 10))->get();
-        //     $flight->passengers()->attach($passengers);
-        // });
     }
 }
