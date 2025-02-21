cadfb254bf49050d1d788a5c3e93ba016d52c5dd
unknown <joekeyrouz03@gmail.com>
2025-02-21 12:58:15 +0200

    Added my project

--- app/Http/Controllers/Api/PassengerController.php
+++ app/Http/Controllers/Api/PassengerController.php
@@ -6,14 +6,17 @@ use App\Models\Flight;
 use App\Models\Passenger;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Database\Eloquent\Builder;
 
 class PassengerController extends Controller
 {
     public function index(Flight $flight){
-        $passengers = Passenger::whereHas('flights', function (Builder $query) use($flight){
-            $query->where('flight_id', "{$flight->id}");
-        })->get();
+        $cacheKey = "passengers_flight_{$flight->id}";
+        $passengers = Cache::Remember($cacheKey,10,function() use($flight){
+                return Passenger::whereHas('flights', function (Builder $query) use($flight){
+                    $query->where('flight_id', "{$flight->id}");
+                })->get();});
         return response()->json($passengers);
     }
 
--- routes/api.php
+++ routes/api.php
@@ -22,12 +22,13 @@ use App\Http\Controllers\Api\ExportUserController;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/users/register', [AuthController::class, 'register']);
 Route::post('/users/logout', [AuthController::class, 'logout'])->middleWare('auth:sanctum');
-Route::get('/users/export', [ExportUserController::class, 'export']);
+Route::get('/flights/{flight}/passengers', [PassengerController::class, 'index']);
 
 Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
-    Route::get('/flights/{flight}/passengers', [PassengerController::class, 'index']);
+
     Route::get('/flights/{flight}/passengers/{passenger}', [PassengerController::class, 'show']);
     Route::get('/flights', [FlightController::class, 'index']);
     Route::get('/flights/{flight}', [FlightController::class, 'show']);
     Route::apiResource('users', UserController::class);
+    Route::get('/users/export', [ExportUserController::class, 'export']);
 });
